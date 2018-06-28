915f3f9eb2cfb2b51b5ee335af3c90699d5234c7
arestch <invfalle@gmail.com>
2018-06-28 14:29:49 +0300

    added bootstrap

--- smovie/src/App.js
+++ smovie/src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from "react";
 import "./App.css";
 import AppMenu from "./components/AppMenu/AppMenu";
+import AppContent from "./components/AppContent/AppContent";
 
 class App extends Component {
   render() {
     return (
       <div className="wrapper">
         <AppMenu />
+        <div className="page-content-wrapper">
+          <div className="container-fluid">
+            <AppContent />
+          </div>
+        </div>
       </div>
     );
   }
