8b0cd0ecf49b1bba6a66a5ff2a774894b54bd0bc
arestch <invfalle@gmail.com>
2018-06-28 14:29:54 +0300

    added appmenu

--- smovie/src/components/AppMenu/AppMenu.js
+++ smovie/src/components/AppMenu/AppMenu.js
@@ -1,40 +1,56 @@
 import React, { Component } from "react";
+import "./AppMenu.css";
 
 class AppMenu extends Component {
   render() {
     return (
-      <div>
-        <div className="sidebar-wrapper">
-          <ul className="sidebar-nav">
-            <li className="sidebar-brand">
-              <a href="#">Start Bootstrap</a>
-            </li>
-            <li>
-              <a href="#">Dashboard</a>
-            </li>
-            <li>
-              <a href="#">Shortcuts</a>
-            </li>
-            <li>
-              <a href="#">Overview</a>
-            </li>
-            <li>
-              <a href="#">Events</a>
-            </li>
-            <li>
-              <a href="#">About</a>
-            </li>
-            <li>
-              <a href="#">Services</a>
-            </li>
-            <li>
-              <a href="#">Contact</a>
-            </li>
-          </ul>
-        </div>
+      <div className="sidebar-wrapper">
+        <ul className="sidebar-nav">
+          <li className="sidebar-brand mx-auto">
+            <a href="#">SMovie</a>
+          </li>
+          <li>
+            <a href="#">Main</a>
+          </li>
+          <li>
+            <a href="#">Movies</a>
+          </li>
+          <li>
+            <a href="#">IMDB</a>
+          </li>
+          <li>
+            <a href="#">Github</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+        </ul>
       </div>
     );
   }
 }
 
 export default AppMenu;
+
+// <div className="sidebar col-sm-2 p-0 m-0">
+// <nav className="nav flex-column nav-pills">
+//   <a className="navbar-brand mb-3 text-light mx-auto" href="#">
+//     SMovie
+//   </a>
+//   <a href="#" className="nav-item nav-link active">
+//     Main
+//   </a>
+//   <a href="#" className="nav-item nav-link bg-dark text-light">
+//     Movies
+//   </a>
+//   <a href="#" className="nav-item nav-link  bg-dark text-light">
+//     IMDB
+//   </a>
+//   <a href="#" className="nav-item nav-link  bg-dark text-light">
+//     Github
+//   </a>
+//   <a href="#" className="nav-item nav-link  bg-dark text-light">
+//     About
+//   </a>
+// </nav>
+// </div>
