95a8fb9c6e01a0a23db838f2f843ed540a5ec37e
arestch <invfalle@gmail.com>
2018-06-29 21:36:55 +0300

    added not found component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import MoviePage from "./components/MoviePage/MoviePage";
 import Movies from "./components/Movies/Movies";
 import AppContent from "./components/AppContent/AppContent";
 import Navbar from "./components/Navbar/Navbar";
+import NotFound from "./components/NotFound/NotFound";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 class App extends Component {
@@ -23,6 +24,7 @@ class App extends Component {
                 <Route exact path="/" component={AppContent} />
                 <Route path="/movie/:id" component={MoviePage} />
                 <Route path="/movies" component={Movies} />
+                <Route path="/not-found" component={NotFound} />
                 <Switch />
               </div>
             </div>
